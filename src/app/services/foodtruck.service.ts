12d16678872d58700b84c393655f458b97728e58
Abril Suarez <suarezabril.as@gmail.com>
2021-02-19 18:10:21 -0300

    no registra foodtruck en la bd why??

--- src/app/services/foodtruck.service.ts
+++ src/app/services/foodtruck.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { LoginService } from '../components/seguridad/login.service';
 import { Foodtruck } from '../models/foodtruck.model';
 
 @Injectable({
@@ -28,39 +29,40 @@ export class FoodtruckService {
         })
       };
 
-    constructor(private http: HttpClient, private router: Router, private loginSerivce: Login){}
+    constructor(private http: HttpClient, private router: Router, private loginService: LoginService){}
 
     public obtenerFoodtruck( id: string ): Observable<Foodtruck>{
         return this.http.get<Foodtruck>('http://localhost:8080/ttps-spring/foodtruck/' + id, this.httpOptions);
     }
 
-    registrarFoodtruck(ft: Foodtruck): void{
-        let usuario = localStorage.getItem('currentUser');
-        
+    registrarFoodtruck(ft: Foodtruck): Observable<Foodtruck>{
+        // let usuario = localStorage.getItem('currentUser');
+        const usuario = this.loginService.getUserLoggedIn();
         console.log(usuario);
-        this.http.post<Foodtruck>(this.baseUrl + '/Foodtruck', ft, this.httpOptions)
+        const idUsuario = usuario.idUsuario;
+        return this.http.post<Foodtruck>(this.baseUrl + '/Foodtruck/' + idUsuario, ft, this.httpOptions);
         // tslint:disable-next-line: deprecation
-        .subscribe( (response: Foodtruck) => {
-            this.foodtruck = {
-                idFoodTruck: response.idFoodTruck,
-                nombre: response.nombre,
-                descripcion: response.descripcion,
-                servicio: response.servicio,
-                whatsapp: response.whatsapp,
-                website: response.website,
-                instagram: response.instagram,
-                twitter: response.twitter,
-                imagenes: response.imagenes,
-            };
-            console.log(this.foodtruck);
-            this.foodtruckSubject.next(this.foodtruck);
-            // this.router.navigate(['/foodtruck/' + this.foodtruck.idFoodTruck]);
-            this.router.navigate(['/home']);
-    });
+    //     .subscribe( (response: Foodtruck) => {
+    //         this.foodtruck = {
+    //             idFoodTruck: response.idFoodTruck,
+    //             nombre: response.nombre,
+    //             descripcion: response.descripcion,
+    //             servicio: response.servicio,
+    //             whatsapp: response.whatsapp,
+    //             website: response.website,
+    //             instagram: response.instagram,
+    //             twitter: response.twitter,
+    //             imagenes: response.imagenes,
+    //         };
+    //         console.log(this.foodtruck);
+    //         this.foodtruckSubject.next(this.foodtruck);
+    //         // this.router.navigate(['/foodtruck/' + this.foodtruck.idFoodTruck]);
+    //         this.router.navigate(['/home']);
+    // });
     }
 
-    borrarFoodtruck( id: string): void{
-        // return this.http.delete(this.baseUrl + '/Foodtruck/' + id, this.httpOptions)
+    borrarFoodtruck( id: string): Observable<any>{
+        return this.http.delete(this.baseUrl + '/Foodtruck/' + id, this.httpOptions);
     }
 
     obtenerActualListener(): Observable<Foodtruck>{
